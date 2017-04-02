f3ee349f0a28fa66a0f8427b1ca315fb34a1fcfd
sfraguas <stella.fraguas@gmail.com>
2017-04-02 16:45:29 -0300

    Update read.php

--- read.php
+++ read.php
@@ -1,7 +1,5 @@
 <?php
 
-  echo "read pagina \\n";
-
   function getUsers()
   {   
     //create connection
@@ -12,24 +10,19 @@
     $result = mysqli_query($conn, $sql);
     
     //show results
-    $return ="oi";
-    echo "antes do if";
+    $return ="";
     if($result) 
     {
-      echo "dentro do if; antes do while";
       while($row = mysqli_fetch_array($result))
       {
-        echo "while";
         $return = $row[USERID];
         echo $row[USERID];
-        echo "depois de userid";
       }
     } else { 
       echo "Não existem usuários";
     }
             
     return $return;
-    
   }
     
 ?>
