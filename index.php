3acaa68669bd0166bd7ab65cd88d8cac3bfd6abc
sfraguas <stella.fraguas@gmail.com>
2017-04-02 15:07:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
   echo "HELLO WORLD!"; 
+  echo "antes do include";
   include "read.php";
+  echo "depois do include";
   $user = getUsers();
+  echo "depois de chamada do metodo";
   echo $user;
+  echo "depois de out do retorno do metodo;
 ?>
