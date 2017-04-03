98985ca5670de5fd8aeb39a6758b418fab2fd7bd
sfraguas <stella.fraguas@gmail.com>
2017-04-03 11:03:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -46,18 +46,6 @@
       </ul>
 
       <section style="padding:20px;margin-top:30px;background-color:#1abc9c;height:1500px;">
-        <?php
-          include methods.php;
-          $isLogged = false;
-          if($isLogged)
-          {
-            echo "<form action="methods.php" method="registerPalestra"> <input type="text" placeholder="Código da palestra"> </br> <input type="submit" value="Registrar-se em palestra"> </form>"
-          } else {
-              echo "<form action="methods.php" method="setUser"> <input type="textbox" placeholder="Nome de Usuário"/> </br> <input type="submit" value="Registrar!"> </form>"
-          }
-        ?>
-
-
         
         <!--<h1>Fixed Top Navigation Bar</h1>
         <h2>Scroll this page to see the effect</h2>
